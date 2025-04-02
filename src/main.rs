7000841db7152b52047f55fb72a6fb4ab1fb86e8
creare <makerbenjammin@gmail.com>
2025-04-02 19:22:41 -0400

    file serving and 404 and latest axum

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
 use axum::{
-    routing::{get, post},
-    Router,
-    Json,
+    response::{Html, IntoResponse}, routing::{get, post}, Json, Router
 };
+use hyper::StatusCode;
 use serde::{Deserialize, Serialize};
+use tower_http::{services::ServeFile, trace::TraceLayer};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     // Build the Axum application
     let app = Router::new()
-        .route("/", get(homepage))
+        .route_service("/", ServeFile::new("assets/index.html"))
         .route("/about", get(about))
-        .route("/contact", post(contact));
+        .route("/contact", post(contact))
+        .fallback(handler_404);
     // Define the address for the server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 35837));
-    println!("Server running at http://{}", addr);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 35838)); // Remember to change this
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     // Run the Axum server
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    axum::serve(listener, app.layer(TraceLayer::new_for_http()))
         .await
         .unwrap();
 }
-// Homepage handler
-async fn homepage() -> &'static str {
-    "Welcome to My Rust Website!"
+
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, Html("<img src=\"https://http.cat/404\" alt=\"http cat 404\"></img>"))
 }
+
+// Homepage handler
+// async fn homepage() -> &'static str {
+//     "Welcome to My Rust Website!"
+// }
+
 // About page handler
 async fn about() -> &'static str {
     "This is the About Page of the Rust Website."
